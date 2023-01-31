7536fd7239f9ba32fa6c647700927105d1a8a661
“Salil48” <salilchharia@gmail.com>
2023-01-31 17:25:45 +0530

    Inserting Values

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -9,3 +9,20 @@ name varchar(200) not null,
 salary float,
 startDate date
 )
+
+--UC-3 Insert values in Table
+
+select * from empoyee_payrolls
+
+Insert into empoyee_payrolls values('Ram',20000,'2022-03-12') 
+
+Insert into empoyee_payrolls values('Mahesh',25000,'2022-04-18')
+
+Insert into empoyee_payrolls values('Gayathri',10000,'2021-05-13')
+
+Insert into empoyee_payrolls values('Aruna',30000,'2021-08-19')
+
+
+
+
+
